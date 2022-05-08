2dd5d3b1d05f596865eba9b735bc36c7bde001fb
uvsq21921706 <71765521+uvsq21921706@users.noreply.github.com>
2022-05-08 20:35:31 +0200

    Delete Server.js

--- DB/Server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-require('./Config/db_config')
-
-const clientModel = require('./Routes/client_route');
-
-app.use('/', clientModel);
-app.listen(5500, ()=> console.log('Server lance'));
