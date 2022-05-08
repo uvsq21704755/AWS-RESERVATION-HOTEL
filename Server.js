7c52f7dbd157fa55c70f5f539c7aeef3241d2553
uvsq21921706 <aouchichesalah1999@gmail.com>
2022-05-08 20:27:04 +0200

    Ajout du server 'classe principale'

---
+++ Server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+const path = require("path");
+const htmlPath = path.join(__dirname, "/Pages");
+app.use(express.static(htmlPath));
+
+app.set('views' , './Pages');
+app.set('view engine', 'ejs');
+
+app.get('/inscription', (req, res) => 
+{
+    res.sendFile(__dirname + "/Pages/signup.html")    
+})
+
+app.get('/connexion_client', (req, res) => 
+{
+    res.sendFile(__dirname + "/Pages/index-connected.html")    
+})
+ 
+app.get('/connexion', (req, res) => 
+{
+    res.sendFile(__dirname + "/Pages/login.html")  
+})
+
+const client_inscription = require('./Routes/client_inscription');
+const client_update = require('./Routes/client_update_info_perso');
+const client_historique = require('./Routes/client_historique');
+const client_consulter_info_perso = require('./Routes/client_consulter_info_perso');
+const client_connecter = require('./Routes/client_connexion');
+
+app.use('/inscription', client_inscription);
+//app.use('/update', client_update);
+app.use('/historique', client_historique);
+
+app.use('/consulter', client_consulter_info_perso);
+app.use('/connexion', client_connecter);
+
+app.listen(5500, ()=> console.log('Server lance'));
